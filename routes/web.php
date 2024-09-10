80992f9679486cfb9afab744a7a2be3ce592070e
EliwahidMdee <155757111+EliwahidMdee@users.noreply.github.com>
2024-09-10 15:13:22 +0300

    fds

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,54 @@
 <?php
-
-
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('homepage');
+     Route::get('/', function () {
+return view('homepage');
+});
 
+Route::get('/doctors', function () {
+return 'Doctors Page';
+});
+
+Route::get('/patients', function () {
+return 'Patients Page';
+});
+
+Route::get('/appointments', function () {
+return 'Appointments Page';
+});
+
+Route::get('/visits', function () {
+return 'Visits Page';
+});
+
+Route::get('/lab-technicians', function () {
+return 'Lab Technicians Page';
+});
+
+Route::get('/lab-tests', function () {
+return 'Lab Tests Page';
+});
+
+Route::get('/pharmacists', function () {
+return 'Pharmacists Page';
+});
+
+Route::get('/prescriptions', function () {
+return 'Prescriptions Page';
+});
+
+Route::get('/cashiers', function () {
+return 'Cashiers Page';
+});
+
+Route::get('/payments', function () {
+return 'Payments Page';
+});
+
+Route::get('/services', function () {
+return 'Services Page';
+});
+
+Route::get('/receptionists', function () {
+return 'Receptionists Page';
 });
